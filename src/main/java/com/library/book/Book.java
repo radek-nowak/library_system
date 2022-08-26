bd13653752db745e50dbd4b34e47d03e72c8a17a
radek-nowak <radeknowak1999@gmail.com>
2022-08-26 15:46:13 +0200

    Update book entity: add comments and delete constructor with all parameters.

--- src/main/java/com/library/book/Book.java
+++ src/main/java/com/library/book/Book.java
@@ -22,40 +22,69 @@ public class Book {
     private String title;
     private String author;
 
+    /**
+     * An empty constructor. For the sake of JPA.
+     */
     public Book() {
     }
 
+    /**
+     * This constructor is used for instantiating a Book object
+     * which is already registered in a database.
+     * @param bookId book ID.
+     */
     public Book(Long bookId) {
         this.bookId = bookId;
     }
 
+    /**
+     * This is a main constructor of this class. It is used when
+     * creating new books. There is no need to pass book ID, since
+     * it is generated automatically.
+     * @param title book title.
+     * @param author book author.
+     */
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
 
-    public Book(Long bookId, String title, String author) {
-        this.bookId = bookId;
-        this.title = title;
-        this.author = author;
-    }
-
+    /**
+     * Returns a book's ID.
+     * @return book ID.
+     */
     public Long getBookId() {
         return bookId;
     }
 
+    /**
+     * Returns a book's title.
+     * @return book title.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets a book title.
+     * @param title book title.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Returns a book's author.
+     * @return book author.
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Sets a book author.
+     * @param author an author of the book.
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
