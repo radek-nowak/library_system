347c8e8a6d8d257b8f19d0adc9d073f3a1185ce9
radek-nowak <radeknowak1999@gmail.com>
2023-12-27 14:07:16 +0100

    book service test

---
+++ src/test/java/com/library/book/BookServiceTest.java
@@ -0,0 +1,48 @@
+package com.library.book;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.library.db.JpaBookStorage;
+import com.library.db.entity.BookEntity;
+import com.library.db.entity.BookRepository;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Import;
+
+@Import({BookMapperImpl.class})
+class BookServiceTest {
+
+  BookService bookService;
+  private BookRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    BookMapper bookMapper = new BookMapperImpl();
+    repository = Mockito.mock(BookRepository.class);
+    BookStorage bookStorage = new JpaBookStorage(repository);
+    bookService = new BookService(bookStorage, bookMapper);
+  }
+
+  @Test
+  void shouldFindBooks() {
+    // given
+    String title = "Catch 22";
+    String author = "Joseph Heller";
+    Mockito.when(repository.findAll()).thenReturn(List.of(new BookEntity(null, title, author)));
+
+    // when
+    List<Book> foundBooks = bookService.findAll();
+
+    // then
+    assertThat(foundBooks)
+        .hasSize(1)
+        .satisfies(
+            books -> {
+              Book book = books.get(0);
+              assertThat(book.author()).isEqualTo(author);
+              assertThat(book.title()).isEqualTo(title);
+            });
+  }
+}
