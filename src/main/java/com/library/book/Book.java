254ac5a59d06c81c97f731346d12d4b900294c38
radek-nowak <radeknowak1999@gmail.com>
2023-12-29 19:13:44 +0100

    no need to override methods

--- src/main/java/com/library/book/Book.java
+++ src/main/java/com/library/book/Book.java
@@ -1,7 +1,6 @@
 package com.library.book;
 
 import com.library.author.Author;
-import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -11,25 +10,5 @@ public record Book(Long id, String title, Set<Author> authors, UUID technicalId)
       authors.add(author);
       author.addBook(this);
     }
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Book book = (Book) o;
-    return Objects.equals(id, book.id)
-        && Objects.equals(title, book.title)
-        && Objects.equals(technicalId, book.technicalId);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, title, technicalId);
-  }
-
-  @Override
-  public String toString() {
-    return "Book{" + "id=" + id + ", title='" + title + '\'' + ", technicalId=" + technicalId + '}';
   }
 }
